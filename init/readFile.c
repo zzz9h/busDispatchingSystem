b7a952c1a10e2d0cf649b8b45288dd9c8dd1d75b
AryCra_07 <90101071+AryCra07@users.noreply.github.com>
2022-05-28 00:34:40 +0800

    Update readFile.c

--- init/readFile.c
+++ init/readFile.c
@@ -1,6 +1,3 @@
-#include "stdio.h"
-#include "stdlib.h"
-
 void readFile() {
     int i = 1;        // i为什么为1？因为13行我们已经处理了head[0]的情况
     char head[3], ch; // head数组分别读取三行的首字母
@@ -25,24 +22,18 @@ void readFile() {
         switch (ch) {
             case 'T':
                 fscanf(fPtr, "%s %s %d", fir, sec, &x); // x获取站点数量
-                printf("Total_Station: %d\n", x);
-                //注意，真实程序中不会有这个printf操作的，printf可以替换成存储等其他操作
+                TOTAL_STATION = x;
                 break;
             case 'S':
                 fscanf(fPtr, "%s %s %s", fir, sec, thir); // thir获取相关策略
-                printf("Strategy: %c\n", thir[0]);
+                STRATEGY = thir[3];
                 //注意，真实程序中不会有这个printf操作的，printf可以替换成存储等其他操作
                 break;
             case 'D':
                 fscanf(fPtr, "%s %s %d", fir, sec, &x); // x获取站点间距离
-                printf("Distance: %d\n", x);
+                GAP = x;
                 //注意，真实程序中不会有这个printf操作的，printf可以替换成存储等其他操作
                 break;
         }
     }
-}
-
-int main(void) {
-    readFile();
-    return EXIT_SUCCESS;
 }
